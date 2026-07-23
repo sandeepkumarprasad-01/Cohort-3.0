2e1b7ff199cd261632a4dfcd4ed4e74863ffada1
sandeepkumarprasad-01 <sprasadkrb@gmail.com>
2026-07-23 08:51:13 +0530

    dom starter

---
+++ Dom/Dom 1/script.js
@@ -0,0 +1,14 @@
+// console.log("heyyy");
+// // textcontent("only changes or updates")
+// const h1 = document.querySelector("h1")
+
+
+// h1.style.color = "red";
+// const isClass =  h1.classList.contains("heading")
+// console.log(isClass);
+
+
+
+
+const bulb = document.querySelector(".bulb")
+const btn = document.querySelector("button")
